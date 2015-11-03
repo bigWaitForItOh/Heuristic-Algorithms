7478cc407b38758d20d69df2cf9655c8058ee6c9
Raghav Dua <duaraghav8@gmail.com>
2015-11-03 14:20:01 +0530

    Update greedy_best_first.py

--- greedy_best_first_search/python/greedy_best_first.py
+++ greedy_best_first_search/python/greedy_best_first.py
@@ -1,6 +1,6 @@
 ###########################################################################################################################
-#IMPLEMENTATION OF GREEDY BEST FIRST SEARCH
-#heuristic parameter is the name of the admissible function greedy_bfs is going to call every time it requires the heuristic value from a particular node to the target node
+#IMPLEMENTATION OF A* SEARCH on a weighted, directed graph.
+#heuristic parameter is the name of the admissible function a_star is going to call every time it requires the heuristic value from a particular node to the target node
 	#example: heuristic ('K', 'Z') = 10 if the heuristic value from node 'K' to target node 'Z' = 10
 #NOTE: heuristic (target) must return 0, since distance between target & itself is always 0
 ###########################################################################################################################
@@ -8,8 +8,8 @@
 from heapq import heapify, heappush, heappop;
 
 def greedy_bfs (graph, start, end, heuristic):
-	current = (0, start);
-	queue, path, backtrace, length = [current], [], {start : (None, 0)}, 0;
+	current = (heuristic (start, end), start);
+	length, path, backtrace, queue = 0, [], {}, [current];
 
 	heapify (queue);
 	while (not current [1] == end):
@@ -18,24 +18,26 @@ def greedy_bfs (graph, start, end, heuristic):
 		except Exception as e:
 			return (-1, []);
 
-		for next_node in graph [current [1]]:
-			heappush (queue, (heuristic (next_node [0], end), next_node [0]));
-			backtrace [next_node [0]] = (current [1], next_node [1]);
+		for node in graph [current [1]]:
+			backtrace [node [0]] = (current [1], node [1]);
+			heappush (queue, (heuristic (node [0], end), node [0]));
 
-	while (not end == None):
-		length += backtrace [end] [1];
-		path, end = [end] + path, backtrace [end] [0];
+	while (not end == start):
+		path = [end] + path;
+		end, length = backtrace [end] [0], length + backtrace [end] [1];
+	path = [start] + path;
 
 	return (length, path);
 
+#EXAMPLE OF HEURISTIC FUNCTION
 def heuristic (current, target):
-	#this heuristic function simply assumes that the target is F, just for simplicity's sake. The heuristic values are made up, but are at most equal to the shortest path, i.e., never overestimate the cost
+	#this heuristic function simply assumes that the target is F, just for simplicity's sake. The heuristic values are made up, but are at most equal to the shortest path, i.e., never overestimate the cost (being admissible implies that)
 	estimates = {
-		'A': 13,
-		'B': 9,
-		'C': 12,
-		'D': 4,
-		'E': 5,
+		'A': 7,
+		'B': 2,
+		'C': 4,
+		'D': 7,
+		'E': 3,
 		'F': 0
 	};
 	return (estimates [current]);
@@ -44,12 +46,13 @@ def heuristic (current, target):
 #REPRESENTED BY A DICTIONARY, WHERE A KEY REPRESENTS THE NODE FROM WHICH THE EDGE STARTS. A key's value is a set of Tuples. The first element of each tuple is the Node the edge reaches. The second element is the weight of the edge. The list contains 1 tuple for every neighbour the KEY has.
 #EXAMPLE: A is connected to B with weight 2 and C with weight 3.
 graph = {
-	'A': set ([ ('B', 2), ('C', 3) ]),
-	'B': set ([ ('C', 3), ('D', 5), ('E', 8) ]),
-	'C': set ([ ('D', 6) ]),
-	'D': set ([ ('E', 8), ('F',6) ]),
-	'E': set ([ ('F', 6) ]),
+	'A': set ([ ('C', 4), ('D', 3), ('E', 8) ]),
+	'B': set ([ ('C', 4), ('F', 2) ]),
+	'C': set ([ ('E', 1), ('F', 5) ]),
+	'D': set ([ ('B', 6) ]),
+	'E': set ([ ('F', 3) ]),
 	'F': set ([])
 };
 
+#SAMPLE CALL TO THE FUNCTION
 print (greedy_bfs (graph, 'A', 'F', heuristic));
